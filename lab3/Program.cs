7b30e5569e08d72da17e80fed57634b25d4ac10f
rockcm <rockcm@etsu.edu>
2023-09-25 20:20:05 -0400

    added collection of games

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -23,9 +23,19 @@ Pokemon Mario = new Pokemon(1047, 1000, " ", "Mario", "poison", "flying", 0, 0, 
 Pokemon Sora = new Pokemon(1048, 1001, " ", "Sora", "poison", "flying", 0, 0, 0, 0, 0, 0, 0, 0);
 Pokemon[] pokearr = { Zelda, Mario };
 BannedGame KH = new BannedGame(1000, "Kingdom Hearts", "KH3", "USA", "Test Ban" );
+BannedGame FF = new BannedGame(1001, "Final Fantasy", "FFVII", "Canada", "Good Game");
+BannedGame W = new BannedGame(1002, "Withcer", "Witcher 3", "Canada", "Best game");
+
+BannedGame[] GameArr = {FF, W};
 
 
 
+Console.WriteLine("Next press will delete all rows from banned games, press any key to continue. ");
+Console.ReadKey();
+
+QB.DeleteAll<BannedGame>();
+Console.WriteLine("All games deleted from banned games, press any key to continue. ");
+Console.ReadKey();
 
 QB.Create(KH);
 Console.WriteLine($" {KH} Added to sql database banned games");
@@ -33,6 +43,8 @@ Console.ReadKey();
 
 QB.Create(Sora);
 Console.WriteLine($" {Sora} Added to sql database pokemon, press any key to continue. ");
+Console.ReadKey();
+Console.WriteLine("Next a collection of pokemon will be added to the DB. ");
 Console.ReadKey();
 
 
@@ -44,16 +56,13 @@ for (int i = 0; i < pokearr.Length; i++)
 
 
 
-
-Console.WriteLine("Next press will delete all rows from banned games, press any key to continue. ");
+Console.WriteLine("Next a collection of banned games will be added to the DB. ");
 Console.ReadKey();
-
-QB.DeleteAll<BannedGame>();
-
-Console.WriteLine("All games deleted from banned games, press any key to continue. ");
-Console.ReadKey();
-
-
+for (int i = 0; i < GameArr.Length; i++)
+{
+    QB.Create(GameArr[i]);
+    Console.WriteLine($"{i} added to sql library");
+}
 
 
 /*
@@ -66,7 +75,7 @@ foreach (var game in GameResults)
 {
     Console.WriteLine(game);
 }
-*/ 
+*/
 
 
 
